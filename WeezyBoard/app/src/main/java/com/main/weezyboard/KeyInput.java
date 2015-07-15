9bf3ad1c5130d715b8555885d630ff4eca3f2a1c
Rohan Jadvani <rj1030@gmail.com>
2015-07-14 23:22:02 -0400

    Setup workspace

---
+++ WeezyBoard/app/src/main/java/com/main/weezyboard/KeyInput.java
@@ -0,0 +1,55 @@
+package com.main.weezyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+
+/**
+ * Created by Rohan Jadvani on 7/14/15.
+ */
+public class KeyInput extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+
+}
